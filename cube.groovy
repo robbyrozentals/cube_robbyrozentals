c939b10336cd83865e8059c4329a330a2b008adf
robert <robert@robert-ThinkPad-T450s>
2022-04-30 15:58:31 -0400

    Ignore the project files

--- cube.groovy
+++ cube.groovy
@@ -1,5 +1,5 @@
 
-return new Cube(	40,// X dimention
+return new Cube(	100,// X dimention
 			60,// Y dimention
 			80//  Z dimention
 			).toCSG()// this converts from the geometry to an object we can work with
